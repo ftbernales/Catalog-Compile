588536af3fcea7b4ff29f2c00e85fcca8fafa0f8
ftbernales <francisjenner.ae@gmail.com>
2023-08-22 22:15:31 +0800

    Add usgs neic downloader and serializer

--- src/catcompile/USGS_save_catalogs.py
+++ src/catcompile/USGS_save_catalogs.py
@@ -3,6 +3,8 @@ import json
 import requests
 from datetime import datetime
 import xml.etree.ElementTree as ET
+import pandas as pd
+
 
 with open(os.path.join(os.path.dirname(__file__), 'coords.json')) as f:
     coords = json.load(f)
@@ -53,6 +55,51 @@ def usgs_save_catalog(file, extension, lat, lon, mindepth, maxdepth, radius):
 
         ET.register_namespace('', "http://www.opengis.net/kml/2.2")
         tree.write(filename)
+
+def download_overall_seismicity(output_dir=None):
+    if output_dir is None:
+        # Use current dir
+        output_dir = os.path.join(os.path.dirname(__file__), 'cat-collections/')
+    
+    file_parent = os.path.join(output_dir, 'Overall_Seismicity_')
+    usgs_save_catalog(file_parent, 'csv', '', '', 0, 800, '')
+    usgs_save_catalog(file_parent, 'kml', '', '', 0, 800, '')
+
+
+def serialize_usgs(filename):
+    """
+    Serialize into new csv format downloaded from USGS Comcat for reading in 
+    HMTK/MBTK
+    """
+    NEW_FILENAME = 'usgs_formatted.csv'
+    df_usgs = pd.read_csv(filename, encoding='windows-1252')
+    
+    # Split datetime into new separate columns
+    df_usgs['time'] = pd.to_datetime(df_usgs['time'])
+    df_usgs.insert(0, "year", df_usgs['time'].dt.year)
+    df_usgs.insert(1, "month", df_usgs['time'].dt.month)
+    df_usgs.insert(2, "day", df_usgs['time'].dt.day)
+    df_usgs.insert(3, "hour", df_usgs['time'].dt.hour)
+    df_usgs.insert(4, "minute", df_usgs['time'].dt.minute)
+    df_usgs.insert(5, "second", df_usgs['time'].dt.second.astype(float) + \
+                            df_usgs['time'].dt.microsecond.astype(float)/1.E6)
+    df_usgs.drop(columns='time', inplace=True)
+
+    # Move ID column
+    id = df_usgs.pop('id')
+    df_usgs.insert(0, 'id', id)
+
+    # Rename fields to be consistent with parsers
+    df_usgs.rename(columns={'mag': 'magnitude', 'magType': 'magnitudeType',
+                            'id': 'eventID'}, 
+                   inplace=True)
+    
+    # Filter events not from USGS National Earthquake Information Center, PDE
+    df_usgs = df_usgs[df_usgs['net'] == 'us']
+
+    # Save to csv
+    df_usgs.to_csv(os.path.join(os.path.dirname(filename), NEW_FILENAME), 
+                   index=False)
 
 
 if __name__ == '__main__':
