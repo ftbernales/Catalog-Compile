3be1e21ffd2d56ae53dfc00e71bb7e85f8ea5300
ftbernales <francisjenner.ae@gmail.com>
2023-08-24 15:57:03 +0800

    Add function default values and condition block

--- src/catcompile/utils/shp_from_coords.py
+++ src/catcompile/utils/shp_from_coords.py
@@ -4,26 +4,41 @@ from shapely.geometry import Polygon
 from catcompile import USGS_save_catalogs as usgs
 
 
-DIRNAME = os.path.dirname(__file__)
-SHP_FILE = 'PH-extents'
+def shp_from_coords(coordinates=None, output_shapefile=None):
+    """
+    Create a polygon shapefile given a list of lon-lat pairs
+    :param coordinates:
+        List of tuples of longitude-latitude pairs
+        (Default: coordinates defined in coords.json)
+    :param output_shapefile
+        String representing path of output shapefile 
+        (Default: PH-extents.shp located in same directory)
+    """
+    if output_shapefile is None:
+        DIRNAME = os.path.dirname(__file__)
+        SHP_FILE = 'PH-extents'
+        # Define the output shapefile path
+        output_shapefile = os.path.join(DIRNAME, SHP_FILE + ".shp")
 
-# Define the coordinates of the polygon's vertices
-coordinates = [
-    (usgs.WEST, usgs.NORTH),
-    (usgs.EAST, usgs.NORTH),
-    (usgs.EAST, usgs.SOUTH),
-    (usgs.WEST, usgs.SOUTH),
-]
+    if coordinates is None:
+        # Define the coordinates of the polygon's vertices
+        coordinates = [
+            (usgs.WEST, usgs.NORTH),
+            (usgs.EAST, usgs.NORTH),
+            (usgs.EAST, usgs.SOUTH),
+            (usgs.WEST, usgs.SOUTH),
+        ]
 
-# Create a Polygon geometry
-polygon = Polygon(coordinates)
+    # Create a Polygon geometry
+    polygon = Polygon(coordinates)
 
-# Create a GeoDataFrame with the polygon geometry
-geometry = [polygon]
-gdf = gpd.GeoDataFrame(geometry=geometry)
+    # Create a GeoDataFrame with the polygon geometry
+    geometry = [polygon]
+    gdf = gpd.GeoDataFrame(geometry=geometry)
 
-# Define the output shapefile path
-output_shapefile = os.path.join(DIRNAME, SHP_FILE + ".shp")
+    # Save the GeoDataFrame to a shapefile
+    gdf.to_file(output_shapefile)
 
-# Save the GeoDataFrame to a shapefile
-gdf.to_file(output_shapefile)
+
+if __name__ == '__main__':
+    shp_from_coords()
